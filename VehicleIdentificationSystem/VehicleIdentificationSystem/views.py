7eda6c32e14e3428faef3600f2ae095639a6cbb5
zzqzzqzzq0816 <2018302110103@whu.edu.cn>
2020-07-12 07:57:43 +0800

    NO2

--- VehicleIdentificationSystem/VehicleIdentificationSystem/views.py
+++ VehicleIdentificationSystem/VehicleIdentificationSystem/views.py
@@ -17,8 +17,9 @@ def download_doc(request):
 
 def online_identification_model(request):
     ctx = {}
+    ctx['path'] = 0
     if request.POST:
-        pic = request.POST['pic']
+        pic = request.FILES.get('picture')
         pic_path = settings.MEDIA_ROOT
         allow_upload = settings.ALLOW_UPLOAD
         if pic.name.split('.')[-1] not in allow_upload:
@@ -27,8 +28,8 @@ def online_identification_model(request):
         with open(pic_path+'/'+pic.name, 'wb') as f:
             for c in pic.chunks():
                 f.write(c)
-        # ctx['name'] = pic.name
-    return render(request,'FunctionPage.html', ctx)
+        ctx['path'] = pic_path+'/'+pic.name
+    return render(request, 'FunctionPage.html', ctx)
 
 
 def online_identification_license(request):
