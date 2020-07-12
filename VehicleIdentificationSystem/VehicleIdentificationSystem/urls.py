4a2deadef956b9f6ba9f26eb709d08f5f630083c
zzqzzqzzq0816 <2018302110103@whu.edu.cn>
2020-07-12 08:59:28 +0800

    NO3

---
+++ VehicleIdentificationSystem/VehicleIdentificationSystem/urls.py
@@ -0,0 +1,29 @@
+"""VehicleIdentificationSystem URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('online-identification/', views.online_identification, name='online-identification'),
+    path('download-packages/', views.download_packages, name='download-packages'),
+    path('download-doc/',views.download_doc, name='download-doc'),
+    path('online-identification/model/', views.online_identification_model, name='online-identification/model'),
+    path('online-identification/license/', views.online_identification_license, name='online-identification/license'),
+    path('online-identification/nums/', views.online_identification_nums, name='online-identification/nums'),
+    path('online-identification/attributes/', views.online_identification_attributes, name='online-identification/attributes'),
+    path('online-identification/destroy/', views.online_identification_destroy, name='online-identification/destroy'),
+    path('online-identification/drive', views.online_identification_drive, name='online-identification/drive'),
+]
